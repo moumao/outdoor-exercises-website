8caf8785e1a2a0310e9d7cbb8bd62b490684774a
moumao <1195358569@qq.com>
2017-03-03 11:47:02 +0800

    updata

--- JS/style.js
+++ JS/style.js
@@ -2,21 +2,6 @@
  * Created by WCY on 2016/11/22.
  */
 
-//载入动画
-window.onload = function(){
-    setTimeout(function(){
-        var loader = document.getElementsByClassName("loader")[0];
-        loader.className="loader fadeout" ;//使用渐隐的方法淡出loading page
-        $("#header").css({
-            position:"fixed"
-        });
-        setTimeout(function(){
-            loader.style.display="none"
-        },1000)
-    },1000);//强制显示loading page 1s
-    $(window).scrollTop(0);
-};
-
 $(document).ready(function () {
 
 
@@ -68,3 +53,17 @@ $(document).ready(function () {
         }
     });
 });
+;(function ($) {
+    $.fn.loadings = function() {
+        setTimeout(function(){
+            var loader = document.getElementsByClassName("loader")[0];
+            loader.className="loader fadeout" ;//使用渐隐的方法淡出loading page
+            $("#header").css({
+                position:"fixed"
+            });
+            setTimeout(function(){
+                loader.style.display="none"
+            },1000)
+        },1000);//强制显示loading page 1s
+    }
+})(jQuery);
